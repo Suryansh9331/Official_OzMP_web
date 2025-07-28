c1aec33f88e3e444a21ecfd474b3b584ebe84d01
suryansh9331 <suryanshmishra9331@gmail.com>
2025-07-28 18:18:48 +0530

    parallax sewtuop done

--- src/Pages/Home/Parallax.jsx
+++ src/Pages/Home/Parallax.jsx
@@ -57,4 +57,48 @@
 //   );
 // }
 
+import React from "react";
+import bgImage4 from "../../assets/images/parallax4.png"; 
 
+export default function ParallaxSection() {
+  return (
+    <section
+      className="relative w-full bg-fixed bg-center bg-cover text-black"
+      style={{
+        backgroundImage: `url(${bgImage4})`,
+        backgroundAttachment: "fixed",
+      }}
+    >
+      {/* Overlay */}
+      <div className="absolute inset-0  z-0"></div>
+
+      {/* Content */}
+      <div className="relative z-10 flex flex-col items-center justify-center lg:items-start  text-center px-2 md:px-0  py-18 md:py-38">
+        <h1
+          className="text-2xl md:text-5xl font-extrabold mb-4 lg:ml-8"
+          style={{ fontFamily: "Archivo, sans-serif" }}
+        >
+          Ready to Take Your Business to the Next Level?
+        </h1>
+        <p
+          className="text-md  md:text-lg  max-w-3xl mb-6 "
+          style={{ fontFamily: "Poppins, sans-serif" }}
+        >
+          Book a free consultation to discover how we can help you grow your
+          business through performance-driven marketing and cutting-edge
+          technology.
+        </p>
+        <button
+          className="px-6 ml-8 mt-4  py-3 rounded-full text-sm font-medium shadow-[4px_4px_0px_#000]"
+          style={{
+            backgroundColor: "#DBD2FE",
+            color: "#000",
+            fontFamily: "Outfit, sans-serif",
+          }}
+        >
+          BOOK YOUR FREE STRATEGY SESSION
+        </button>
+      </div>
+    </section>
+  );
+}
