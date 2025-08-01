1ab1d9a8c8487e9c3bfcb5bc7c6d1d96bd941824
suryansh9331 <suryanshmishra9331@gmail.com>
2025-08-01 23:53:12 +0530

    Digital marketing start v1

---
+++ src/Pages/DigitalMarketing.jsx/DigitalMarketing.jsx
@@ -0,0 +1,18 @@
+
+
+
+import Hero from "./Hero"
+
+
+
+const DigitalMarketing = () => {
+  return (
+    <div className= " w-full overflow-hidden">
+      
+      <Hero/>
+     
+    </div>
+  );
+};
+
+export default DigitalMarketing;
