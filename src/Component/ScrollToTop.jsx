08e33b8e77a279cbd4291e93e18ff77169cf8c1a
suryansh9331 <suryanshmishra9331@gmail.com>
2025-08-04 02:23:58 +0530

    refresh issue resolved

--- src/Component/ScrollToTop.jsx
+++ src/Component/ScrollToTop.jsx
@@ -1,17 +1,15 @@
 import { useEffect } from "react";
-import { useLocation, useNavigationType } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
-  const location = useLocation();
-  const navigationType = useNavigationType();
+  const { pathname } = useLocation();
 
   useEffect(() => {
-    // Scroll to top on all navigations, even if path doesn't change
     window.scrollTo({
       top: 0,
       behavior: "smooth",
     });
-  }, [location.key, navigationType]); // Trigger on each navigation
+  }, [pathname]); // This will run on every route change and refresh
 
   return null;
 };
