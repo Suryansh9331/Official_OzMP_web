3209525a4b06cd371fcfbaf9368ab57670854928
suryansh9331 <suryanshmishra9331@gmail.com>
2025-08-05 23:49:48 +0530

    Blog Page v2

--- src/Routing/Routes.jsx
+++ src/Routing/Routes.jsx
@@ -9,8 +9,8 @@ import Footer from "../Component/Footer";
 import About from "../Pages/About/About";
 
 import ScrollToTopButton from "../Component/SttButton";
-import DigitalMarketing from "../Pages/DigitalMarketing/DigitalMarketing"
-import Contact from "../Pages/Contact/Contact"
+import DigitalMarketing from "../Pages/DigitalMarketing/DigitalMarketing";
+import Contact from "../Pages/Contact/Contact";
 import OurTeam from "../Pages/Team/Team";
 import WebsiteDevelopmentPage from "../Pages/WebDev/WebDevelopment";
 import APIDevelopmentPage from "../Pages/APIDevelopement/APIDevelopmentPage";
@@ -19,32 +19,57 @@ import MobileAppDevelopmentPage from "../Pages/MobileApp/MobileAppDevelopmentPag
 import GraphicUIUXDesignPage from "../Pages/GraphicUIUX/GraphicUIUXDesignPage";
 import ECommerceDevelopmentPage from "../Pages/Ecommerce/ECommerceDevelopmentPage";
 import ITPage from "../Pages/IT/ITPage";
+import Blog from "../Pages/Blog/Blog";
 const AppRoutes = () => {
   return (
     <div>
-    
-    
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about-us" element={<About/>} />
-        <Route path="/services-digital-marketing" element={<DigitalMarketing/>} />
-        <Route path="/contact-us" element={<Contact/>} />
-        <Route path="/our-team" element={<OurTeam/>} />
-        
+        <Route path="/about-us" element={<About />} />
+        <Route
+          path="/services-digital-marketing"
+          element={<DigitalMarketing />}
+        />
+        <Route path="/contact-us" element={<Contact />} />
+        <Route path="/our-team" element={<OurTeam />} />
+
         {/* routes for IT services */}
         <Route path="/it-services" element={<ITPage />} />
-        <Route path="/it-services/website-developement" element={<WebsiteDevelopmentPage />} />
-        <Route path="/it-services/api-developement" element={<APIDevelopmentPage/>}/>
-        <Route path="/it-services/software-developement" element={<SoftwareDevelopmentPage/>}/>
-        <Route path="/it-services/mobile-application-developement" element={<MobileAppDevelopmentPage/>} />
-        <Route path="/it-services/graphics-uiux-developement" element={<GraphicUIUXDesignPage/>} />
-        <Route path="/it-services/e-commerce-solution" element={<ECommerceDevelopmentPage/>} />
+        <Route
+          path="/it-services/website-developement"
+          element={<WebsiteDevelopmentPage />}
+        />
+        <Route
+          path="/it-services/api-developement"
+          element={<APIDevelopmentPage />}
+        />
+        <Route
+          path="/it-services/software-developement"
+          element={<SoftwareDevelopmentPage />}
+        />
+        <Route
+          path="/it-services/mobile-application-developement"
+          element={<MobileAppDevelopmentPage />}
+        />
+        <Route
+          path="/it-services/graphics-uiux-developement"
+          element={<GraphicUIUXDesignPage />}
+        />
+        <Route
+          path="/it-services/e-commerce-solution"
+          element={<ECommerceDevelopmentPage />}
+        />
         {/* <Route path="/it-services/hosting-and-server" element={</>} />
         <Route path="/it-services/techsupport-amc-services" element={<OurTeam/>} /> */}
+
+        <Route
+          path="/blogs"
+          element={<Blog />}
+        />
       </Routes>
-      <Footer/>
-        <ScrollToTopButton/>
+      <Footer />
+      <ScrollToTopButton />
     </div>
   );
 };
