66d33c6d259610572b7c6d6c217f20708d8bcd6e
suryansh9331 <suryanshmishra9331@gmail.com>
2025-08-13 23:37:27 +0530

    brands removed

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,10 @@
 
 
 
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import CaseStudiesSection from "./CaseStudy";
 import HeroBanner from "./Hero";
 import IndustrySection from "./IndustrySection";
-import TrustedBrands from "./Partners";
 import StatsSection from "./Reviews";
 import ServicesOverview from "./Services";
 import WhoWeAre from "./WhoWeAre";
@@ -29,7 +28,6 @@ const Home = () => {
       <div className="lg:px-16 px-2 md:px-4 mb-6 mt-6 md:mt-2">
         <StatsSection />
       </div>
-      <TrustedBrands />
       <WhoWeAre />
       <ServicesOverview />
       <IndustrySection />
--- src/Pages/Home/Partners.jsx
+++ src/Pages/Home/Partners.jsx
@@ -1,61 +1,61 @@
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 
-const logos = [
+// const logos = [
  
-  "/images/google.png",
-  "/images/Acer.png",
-  "/images/Huawai.png",
-  "/images/intel.png",
-"/images/Asus.png",
-  "/images/oracle.png",
-  "/images/Svmsung.png",
-  "/images/techM.png",
+//   "/images/google.png",
+//   "/images/Acer.png",
+//   "/images/Huawai.png",
+//   "/images/intel.png",
+// "/images/Asus.png",
+//   "/images/oracle.png",
+//   "/images/Svmsung.png",
+//   "/images/techM.png",
   
-];
+// ];
 
-const TrustedBrands = () => {
-  return (
-    <section className="py-16 bg-white overflow-hidden w-full">
-      {/* Heading */}
-      <div className="text-center mb-8">
-        <h2
-          className="text-2xl md:text-4xl font-semibold text-black"
-          style={{ fontFamily: "Archivo, sans-serif" }}
-        >
-          Trusted by Industry Leaders
-        </h2>
-        <p
-          className="text-gray-700 text-sm md:text-base mt-4"
-          style={{ fontFamily: "Roboto, sans-serif" }}
-        >
-          We’ve delivered measurable results for startups, SMEs, and multinational companies.
-        </p>
-      </div>
+// const TrustedBrands = () => {
+//   return (
+//     <section className="py-16 bg-white overflow-hidden w-full">
+//       {/* Heading */}
+//       <div className="text-center mb-8">
+//         <h2
+//           className="text-2xl md:text-4xl font-semibold text-black"
+//           style={{ fontFamily: "Archivo, sans-serif" }}
+//         >
+//           Trusted by Industry Leaders
+//         </h2>
+//         <p
+//           className="text-gray-700 text-sm md:text-base mt-4"
+//           style={{ fontFamily: "Roboto, sans-serif" }}
+//         >
+//           We’ve delivered measurable results for startups, SMEs, and multinational companies.
+//         </p>
+//       </div>
 
-      {/* Marquee Animation */}
-      <div className="relative w-full">
-        <motion.div
-          className="flex gap-12 items-center whitespace-nowrap mt-4"
-          animate={{ x: ["0%", "-50%"] }}
-          transition={{
-            repeat: Infinity,
-            ease: "linear",
-            duration: 20,
-          }}
-        >
-          {/* Duplicate logos for seamless infinite loop */}
-          {[...logos, ...logos].map((logo, index) => (
-            <img
-              key={index}
-              src={logo}
-              alt={`logo-${index}`}
-              className=" md:h-10 h-6 lg:h-10 w-auto object-contain"
-            />
-          ))}
-        </motion.div>
-      </div>
-    </section>
-  );
-};
+//       {/* Marquee Animation */}
+//       <div className="relative w-full">
+//         <motion.div
+//           className="flex gap-12 items-center whitespace-nowrap mt-4"
+//           animate={{ x: ["0%", "-50%"] }}
+//           transition={{
+//             repeat: Infinity,
+//             ease: "linear",
+//             duration: 20,
+//           }}
+//         >
+//           {/* Duplicate logos for seamless infinite loop */}
+//           {[...logos, ...logos].map((logo, index) => (
+//             <img
+//               key={index}
+//               src={logo}
+//               alt={`logo-${index}`}
+//               className=" md:h-10 h-6 lg:h-10 w-auto object-contain"
+//             />
+//           ))}
+//         </motion.div>
+//       </div>
+//     </section>
+//   );
+// };
 
-export default TrustedBrands;
+// export default TrustedBrands;
