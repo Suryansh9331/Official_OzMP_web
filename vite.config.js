e4666eaf5897469ca7d401983f66f3c89e3f3c23
Suryansh9331 <suryanshmishra9331@gmail.com>
2025-08-15 08:18:01 +0000

    vite build code

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+    allowedHosts: ['www.ozmediaplanet.com', 'ozmediaplanet.com']
+  }
 })
