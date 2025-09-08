369e366d3789d48f0597097c6fec43a88aba0ccd
suryansh9331 <suryanshmishra9331@gmail.com>
2025-09-08 23:54:24 +0530

    unwanted code removed 3

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -1,9 +1,5 @@
 import React from "react";
-// import bgimage from "../../assets/images/bg.png";
-// import blog1 from "../../assets/images/Blogbanner1.png";
-// import section1 from "../../assets/images/blogsection1.png";
-// import section2 from "../../assets/images/section2blog.png";
-// import section3 from "../../assets/images/section3.png";
+
 import HeroBanner from "./Hero";
 import SidebarWidget from "./Sidebar";
 import BlogCardList from "./Blogcard";
