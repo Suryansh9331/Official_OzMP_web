71c4f29d1431d32749299f8b952abfcc33344597
suryansh9331 <suryanshmishra9331@gmail.com>
2025-09-09 23:28:10 +0530

    code updated

--- src/Pages/Blog/Hero.jsx
+++ src/Pages/Blog/Hero.jsx
@@ -9,7 +9,6 @@ const HeroBanner = () => {
       className="w-full bg-[#DBD2FF]   md:p relative overflow-hidden px-4 lg:px-0 md:px-10 md:pb-6  lg:pb-28 lg:pr-12  shadow-[10px_12px_0px_rgb(0,0,0,0.6)] "
       style={{ fontFamily: "Poppins, sans-serif" }}
     >
-      {/* Background image */}
       <div
         className="absolute inset-0 bg-no-repeat bg-cover bg-center opacity-50 z-0"
         style={{
@@ -19,9 +18,7 @@ const HeroBanner = () => {
         }}
       ></div>
 
-      {/* Main Content */}
       <div className="relative z-10 max-w-full mx-auto flex flex-col-reverse  md:flex-col-reverse  lg:flex-row items-center justify-between">
-        {/* Left Text Section */}
         <div className="lg:w-1/2 md:w-full w-full mb-8 md:mb-0 lg:mx-12  lg:px-0  md:px-12  px-4 py-10 md:py-8 ">
           <h1
             className="text-3xl md:text-5xl font-semibold mb-4 text-gray-900"
@@ -52,7 +49,6 @@ const HeroBanner = () => {
           </ul>
         </div>
 
-        {/* Right Image Section */}
         <div className="lg:w-1/2  w-full flex justify-center z-10 md:shadow-[10px_12px_0px_rgb(0,0,0,0.6)]   shadow-[8px_12px_0px_rgb(0,0,0,0.8)]">
           <video
             src={heroVideo}
